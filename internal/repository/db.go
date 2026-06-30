7c898ad8baadd2046c29301387e50c100bb1cc3e
itz_pabloo <yurii.alekhin@yandex.ru>
2026-06-30 22:10:19 +0300

    Load database config from .env file

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -1,14 +1,31 @@
 package repository
 
 import (
+	"fmt"
 	"log"
+	"os"
+
 	"github.com/itspabloo/hermes/internal/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=localhost user=hermes_user password=secret_pass dbname=hermes_db port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+	
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
